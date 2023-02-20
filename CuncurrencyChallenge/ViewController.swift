5f89fe906d2cf6cb18897989160d948478a802f0
sangjin <larger93r@gmail.com>
2023-02-20 23:14:55 +0900

    Initial Commit

---
+++ CuncurrencyChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CuncurrencyChallenge
+//
+//  Created by 광리자 on 2023/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
