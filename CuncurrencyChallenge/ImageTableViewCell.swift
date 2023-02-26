da24cfe7911c93ceaafb14180e7525ff250a4d60
sangjin <larger93r@gmail.com>
2023-02-26 16:25:38 +0900

    progressView 추가

--- CuncurrencyChallenge/ImageTableViewCell.swift
+++ CuncurrencyChallenge/ImageTableViewCell.swift
@@ -12,6 +12,7 @@ class ImageTableViewCell: UITableViewCell {
     
     let myImageView = UIImageView()
     let loadButton = UIButton()
+    let progressView = UIProgressView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -26,18 +27,25 @@ class ImageTableViewCell: UITableViewCell {
     private func setupView() {
         self.addSubview(myImageView)
         self.addSubview(loadButton)
+        self.addSubview(progressView)
         
         myImageView.translatesAutoresizingMaskIntoConstraints = false
         myImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
         myImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
         myImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-        myImageView.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        myImageView.widthAnchor.constraint(equalToConstant: 80).isActive = true
         
         loadButton.translatesAutoresizingMaskIntoConstraints = false
         loadButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
         loadButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
         loadButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
         loadButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        progressView.leadingAnchor.constraint(equalTo: self.myImageView.trailingAnchor, constant: 10).isActive = true
+        progressView.trailingAnchor.constraint(equalTo: self.loadButton.leadingAnchor, constant: -10).isActive = true
+        progressView.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
         myImageView.backgroundColor = .blue
         loadButton.setTitle("다운로드", for: .normal)
@@ -49,18 +57,36 @@ class ImageTableViewCell: UITableViewCell {
         guard let url = URL(string: stringUrl) else {
             return
         }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { data, _, _ in
-            guard let data = data,
-                  let image = UIImage(data: data) else {
-                return
-            }
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        let downloadTask = session.downloadTask(with: url)
+        downloadTask.resume()
+    }
+}
 
-            DispatchQueue.main.async() {[weak self] in
-                self?.myImageView.image = image
-            }
-        }.resume()
+extension ImageTableViewCell: URLSessionDownloadDelegate {
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        // Get the data from the download location
+        guard let imageData = try? Data(contentsOf: location) else {
+            print("Error retrieving image data")
+            return
+        }
+        
+        // Update the UI on the main thread with the downloaded image
+        DispatchQueue.main.async {
+            self.myImageView.image = UIImage(data: imageData)
+        }
+    }
+    
+    
+    // This method is called periodically as the download progresses
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        // Calculate the download progress as a fraction of the total bytes
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        
+        // Update the progress bar on the main thread
+        DispatchQueue.main.async {
+            self.progressView.progress = progress
+        }
     }
 }
--- CuncurrencyChallenge/ViewController.swift
+++ CuncurrencyChallenge/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(ImageTableViewCell.self, forCellReuseIdentifier: ImageTableViewCell.identifier)
         tableView.rowHeight = 100
         
@@ -32,14 +33,21 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.identifier, for: indexPath) as! ImageTableViewCell
-        cell.configureImage(stringUrl: "https://images.velog.io/images/sdb016/post/34bdac57-2d63-43ce-a14c-8054e9e036de/test.png")
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? ImageTableViewCell else {
+            return
+        }
+        cell.configureImage(stringUrl: "https://www.treeinfo.net/data/file/ti_free/thumb-2001093550_ZYiXBxHv_acc30dee7d6f41bcd70cdde0f16214b6d21893d4_700x467.jpg")
     }
 }
